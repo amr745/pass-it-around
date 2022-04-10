368f6efa946d8b9302185914ae9f6ec345fa0a2e
Ashley Richard <ashleymrichard@gmail.com>
2022-04-10 08:52:43 -0700

    loop 99 working

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-const express = require("express")
+const express = require("express");
+// const res = require("express/lib/response");
 const app = express()
 const port = 3000
 
+// const zeroHome = <a href="https://localhost3000/">start over</a>;
+// let lessBeer = <a href = "https://localhost3000/i">take one down, pass it around</a>;
+
 app.get("/", (req,res) => {
-    res.send("99 Bottles of beer on the wall")
+    for (let i = 99; i > 0; i--) {
+        if(i == 99) {
+            res.send(`${i} Bottles of beer on the wall`)
+        }
+    }
 });
-
-
+    
+    
+    
+    //     if (i == 0) {
+    //         // res.send(zeroHome)
+    //         res.send("stuff")
+    //     } else {
+    //             // res.send(lessBeer)
+    //             res.send("more stuff")
+    //         }
+    //     }
+    // });
 
 app.listen(port, () => {
     console.log("listening on port", port)
