48650469001222cb4941eea80743130814ac0839
Ashley Richard <ashleymrichard@gmail.com>
2022-04-11 11:26:22 -0700

    98 Bottles of Beer on the Wall

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ const app = express()
 const port = 3000
 
 app.get("/", (req,res) => {
-    for (let i = 99; i >= 0; i--) {
+    res.send(`99 Bottles of beer on the wall <a href=${"http://localhost:3000/:number_of_bottles"}>take one down pass it around<a>`)
+});
+
+app.get("/:number_of_bottles", (req,res) => {
+    for (let i = 98; i >= 0; i--) {
         // console.log(i)
         if (i == 0) {
         res.send(`<a href=${"http://localhost:3000/"}>start over</a>`)
         // console.log(`<a href=${"https://localhost3000/"}>start over</a>`)
     } else {
-        const count = i-1
-        res.send(`${i} Bottles of beer on the wall <a href=${"http://localhost:3000/"}>take one down pass it around<a>`)
+        // const number_of_bottles = i-1
+        res.send(`${i} Bottles of beer on the wall <a href=${"http://localhost:3000/:number_of_bottles"}>take one down pass it around<a>`)
         }
     }
 });
+
+// app.get("/:number_of_bottles" (req,res) => {
+
+// })
 
 app.listen(port, () => {
     console.log("listening on port", port)
